01644cfe647affceb6fa5cc490009f74541cf399
Arturo Zepeda <aaze92@gmail.com>
2023-09-17 20:46:25 -0600

    remuevo documentos

--- jsonController.js
+++
@@ -1,8 +0,0 @@
-const json = require('jsonfile');
-
-const leeJSON = (req) => {
-    const workbook = json.readFile(req);  // Step 2
-    return workbook;
-};
-
-console.log(leeJSON('./uploads/Documento prueba.json'));
